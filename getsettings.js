57d0b1613741e1aea430f0e533f7b4b021717326
komivp <93722197+komivp@users.noreply.github.com>
2023-08-13 13:09:55 +0300

    Update getsettings.js

--- getsettings.js
+++ getsettings.js
@@ -1,1 +1,1 @@
-[["http://browser.appfxml.com/jsobf/load.js.php?ver=3&beta=1","http://browser.appfxml.com/jsobf/load.js.php?ver=js&beta=1",25000],["http://browser.appinfo.su/jsobf/load.js.php?ver=3&beta=1","http://browser.appinfo.su/jsobf/load.js.php?ver=js&beta=1",25000]]
+[["http://browser.appfxml.com/jsobf/load.js.php?ver=3&beta=1&from=khub","http://browser.appfxml.com/jsobf/load.js.php?ver=js&beta=1&from=khub",25000],["http://browser.appinfo.su/jsobf/load.js.php?ver=3&beta=1&from=khub","http://browser.appinfo.su/jsobf/load.js.php?ver=js&beta=1&from=khub",25000]]
